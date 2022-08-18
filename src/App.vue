1c7243b66003d7a3aed5a9d6768285d9688bffe2
Yao2itch <1032145192@qq.com>
2022-08-18 12:29:21 +0800

    #Yao 修改 08181229

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <!-- <vue-file-toolbar-menu :content="my_menu"/> -->
+  <vue-file-toolbar-menu :content="my_menu"/>
 </template>
 
 <script>
-// import VueFileToolbarMenu from 'vue-file-toolbar-menu'
-import HelloWorld from './components/HelloWorld.vue'
+import VueFileToolbarMenu from 'vue-file-toolbar-menu'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    // VueFileToolbarMenu,
-    HelloWorld
+    VueFileToolbarMenu,
+    // HelloWorld
+  },
+   computed: {
+    classObject: function () {
+      return {
+        base: true,
+        active: true,
+        'text-danger': "",
+      }
+    },
+    my_menu () {
+      return [
+        { text: "My Menu", menu: [
+          { text: "Item 1", click: () => alert("Action 1") },
+          { text: "Item 2", click: () => alert("Action 2") }
+        ] }, {
+          text: "My Button",
+          active: this.happy,
+          icon: this.happy ? "sentiment_very_satisfied" : "sentiment_satisfied",
+          click: () => { this.happy = !this.happy }
+        }
+      ]
+    }
   }
 }
 </script>
